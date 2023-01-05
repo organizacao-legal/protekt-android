49ccc2f656af56eef8f6fa096ff96b594877034b
GabrielBrisson <gabrielbrisson02@gmail.com>
2023-01-05 19:42:01 -0300

    Adding new colors

--- app/src/main/java/me/brisson/protekt/ui/theme/Color.kt
+++ app/src/main/java/me/brisson/protekt/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package me.brisson.protekt.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val LightBlue = Color(0xFF3B82F6)
+val LighterBlue = Color(0xFFECFEFF)
+val White = Color(0xFFF5F5F5)
+val Black = Color(0xFF131313)
+val Red = Color(0xFFEF4444)
+val Green = Color(0xFF22C55E)
+val Yellow = Color(0xFFFACC15)
+val LightGray = Color(0xFFD4D4D4)
+val DarkGray = Color(0xFF737373)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
--- app/src/main/java/me/brisson/protekt/ui/theme/Theme.kt
+++ app/src/main/java/me/brisson/protekt/ui/theme/Theme.kt
@@ -12,15 +12,23 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.ViewCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = LightBlue,
+    onPrimary = LighterBlue,
+    background = Black,
+    onBackground = White,
+    surface = Black,
+    onSurface = White,
+    error = Red
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightBlue,
+    onPrimary = LighterBlue,
+    background = Black,
+    onBackground = White,
+    surface = Black,
+    onSurface = White,
+    error = Red
 )
 
 @Composable
